95b3b2fe50ae21cdbec941a3c10f94dd80d32b92
Vladislav Polishchuk <vladpolisuk159@gmail.com>
2021-12-12 17:36:46 +0300

    feature: fonts has been changed

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import '@fontsource/inter';
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import theme from './theme';
 
 const AppComponent = () => {
 	return (
 		<StrictMode>
-			<App />
+			<ChakraProvider resetCSS theme={theme}>
+				<App />
+			</ChakraProvider>
 		</StrictMode >
 	)
 }
---
+++ src/theme.ts
@@ -0,0 +1,29 @@
+import { extendTheme, ThemeConfig } from '@chakra-ui/react';
+
+const config: ThemeConfig = {
+	initialColorMode: 'light',
+	useSystemColorMode: false
+}
+
+const fonts = {
+	body: 'Roboto, sans-serif',
+	heading: 'Inter, sans-serif',
+}
+
+const styles = {
+	body: {
+		padding: 0,
+		margin: 0
+	},
+	heading: {
+		padding: 0,
+	}
+} 
+
+const theme = extendTheme({
+	config,
+	fonts,
+	styles
+});
+
+export default theme;
