471f80f9edd8d49d2f0bcf0bea3194f13d4e6e8a
Vladislav Polishchuk <vladpolisuk159@gmail.com>
2021-12-14 16:54:53 +0300

    chore: render notes list or empty workboard

--- src/components/Workboard/index.tsx
+++ src/components/Workboard/index.tsx
@@ -1,69 +1,81 @@
-import { SimpleGrid } from '@chakra-ui/layout';
 import React from 'react'
+import { SimpleGrid } from '@chakra-ui/layout';
 import { Note } from './Note/Note';
 
 const Workboard = () => {
+    const notesList: Array<{ title: string; content: string; isEmpty: boolean }> = [
+        {
+            title: 'Go to the university at 10:30am',
+            content: ` Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Doloribus tenetur officiis pariatur veniam sunt, magnam ipsum 
+            voluptates reprehenderit, alias error ad dolorum dolor repellat 
+            inventore quas architecto nulla officia quia.`,
+            isEmpty: false
+        },
+        {
+            title: 'Go to the university at 10:30am',
+            content: ` Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Doloribus tenetur officiis pariatur veniam sunt, magnam ipsum 
+            voluptates reprehenderit, alias error ad dolorum dolor repellat 
+            inventore quas architecto nulla officia quia.`,
+            isEmpty: false
+        },
+        {
+            title: 'Go to the university at 10:30am',
+            content: ` Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Doloribus tenetur officiis pariatur veniam sunt, magnam ipsum 
+            voluptates reprehenderit, alias error ad dolorum dolor repellat 
+            inventore quas architecto nulla officia quia.`,
+            isEmpty: false
+        },
+        {
+            title: 'Go to the university at 10:30am',
+            content: ` Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Doloribus tenetur officiis pariatur veniam sunt, magnam ipsum 
+            voluptates reprehenderit, alias error ad dolorum dolor repellat 
+            inventore quas architecto nulla officia quia.`,
+            isEmpty: false
+        },
+        {
+            title: 'Go to the university at 10:30am',
+            content: ` Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Doloribus tenetur officiis pariatur veniam sunt, magnam ipsum 
+            voluptates reprehenderit, alias error ad dolorum dolor repellat 
+            inventore quas architecto nulla officia quia.`,
+            isEmpty: false
+        },
+        {
+            title: 'Go to the university at 10:30am',
+            content: ` Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Doloribus tenetur officiis pariatur veniam sunt, magnam ipsum 
+            voluptates reprehenderit, alias error ad dolorum dolor repellat 
+            inventore quas architecto nulla officia quia.`,
+            isEmpty: false
+        },
+        {
+            title: 'Go to the university at 10:30am',
+            content: ` Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Doloribus tenetur officiis pariatur veniam sunt, magnam ipsum 
+            voluptates reprehenderit, alias error ad dolorum dolor repellat 
+            inventore quas architecto nulla officia quia.`,
+            isEmpty: false
+        }
+    ]
+
+    const notesComponentlist = notesList.map(({ title, content, isEmpty }) => {
+        return <Note title={title} content={content} isEmpty={isEmpty} />
+    })
+
     return (
         <SimpleGrid
             p='15px'
             columns={4}
             spacing='15px'
             overflow='auto'
-            minChildWidth={250}>
-            <Note
-                title={'Take a shower'}
-                content={`Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Fugit saepe porro obcaecati, eum sit voluptas dolorem reprehenderit
-                dolorum a accusamus error, provident maxime sequi laborum esse?
-                Neque quia eaque error.`} />
-            <Note
-                title={'Go to the bathroom at 6:30 pm'}
-                content={`Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Quasi impedit quis dolores ipsa et quod molestias esse facere officia, 
-                similique deleniti provident aut nihil ratione nam ex. 
-                Labore, asperiores quos!`} />
-            <Note
-                title={'Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla'}
-                content={`Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Quasi impedit quis dolores ipsa et quod molestias esse facere officia, 
-                similique deleniti provident aut nihil ratione nam ex. 
-                Labore, asperiores quos!`} />
-            <Note
-                title={'Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla'}
-                content={`Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Quasi impedit quis dolores ipsa et quod molestias esse facere officia, 
-                similique deleniti provident aut nihil ratione nam ex. 
-                Labore, asperiores quos!`} />
-            <Note
-                title={'Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla'}
-                content={`Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Quasi impedit quis dolores ipsa et quod molestias esse facere officia, 
-                similique deleniti provident aut nihil ratione nam ex. 
-                Labore, asperiores quos!`} />
-            <Note
-                title={'Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla'}
-                content={`Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Quasi impedit quis dolores ipsa et quod molestias esse facere officia, 
-                similique deleniti provident aut nihil ratione nam ex. 
-                Labore, asperiores quos!`} />
-            <Note
-                title={'Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla'}
-                content={`Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Quasi impedit quis dolores ipsa et quod molestias esse facere officia, 
-                similique deleniti provident aut nihil ratione nam ex. 
-                Labore, asperiores quos!`} />
-            <Note
-                title={'Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla'}
-                content={`Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Quasi impedit quis dolores ipsa et quod molestias esse facere officia, 
-                similique deleniti provident aut nihil ratione nam ex. 
-                Labore, asperiores quos!`} />
-            <Note
-                title={'Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla'}
-                content={`Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Quasi impedit quis dolores ipsa et quod molestias esse facere officia, 
-                similique deleniti provident aut nihil ratione nam ex. 
-                Labore, asperiores quos!`} />
+            minChildWidth={280}
+            height='fit-content'
+            maxHeight='100%'>
+            {notesList.length ? notesComponentlist : ''}
         </SimpleGrid>
     )
 }
