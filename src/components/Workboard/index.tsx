480ed0aeba80a22fa2821df88efc9d317b352e08
Vladislav Polishchuk <vladpolisuk159@gmail.com>
2021-12-14 13:05:55 +0300

    feature: Component workboard has been created

---
+++ src/components/Workboard/index.tsx
@@ -0,0 +1,12 @@
+import { Box, Grid, GridItem, Heading } from '@chakra-ui/layout'
+import React from 'react'
+
+const Workboard = () => {
+    return (
+        <Grid p='15px'>
+            <GridItem />
+        </Grid>
+    )
+}
+
+export default Workboard
