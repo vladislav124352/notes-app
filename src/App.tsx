f0ec613ef946763af772495b7904f19af6c75674
Vladislav Polishchuk <vladpolisuk159@gmail.com>
2021-12-14 13:13:09 +0300

    feature: Added sidebar and workboard components to the "App" component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Box } from '@chakra-ui/layout';
+import { Grid } from '@chakra-ui/layout';
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Workboard from './components/Workboard';
+import { useBreakpointValue } from '@chakra-ui/media-query';
 
 const App = () => {
+	const sidebarWidthBreakpoint = useBreakpointValue({ base: '70px 1fr', lg: '235px 1fr' })
+
 	return (
-		<Box>
+		<Grid templateColumns='1fr' templateRows='60px 1fr' h='100vh'>
 			<Header />
-		</Box>
+			<Grid as='main' templateColumns={sidebarWidthBreakpoint}>
+				<Sidebar />
+				<Workboard />
+			</Grid>
+		</Grid>
 	);
 }
 
