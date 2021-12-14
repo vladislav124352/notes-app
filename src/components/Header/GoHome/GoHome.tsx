d096c1523ef03f5955d78d7923f2d0ce4753887a
Vladislav Polishchuk <vladpolisuk159@gmail.com>
2021-12-14 14:52:42 +0300

    fix: changed responsive header logic

--- src/components/Header/GoHome/GoHome.tsx
+++ src/components/Header/GoHome/GoHome.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
-import { Box, Grid, Heading, LinkBox, LinkOverlay } from '@chakra-ui/layout'
+import { Box, Grid, Heading, Link, LinkBox, LinkOverlay } from '@chakra-ui/layout'
 import NotesAppIcon from '../images/NotesApp-icon.svg';
 import { Image } from '@chakra-ui/image';
 import { useBreakpointValue } from '@chakra-ui/media-query';
 
 export const GoHome = () => {
-    const logoTextDisplayBreakpoint = useBreakpointValue({ base: 'none', lg: 'flex' })
-    const goHomeGridBreakpoint = useBreakpointValue({ base: '40px', lg: '40px 1fr' })
+    const isCollapseBreakpoint = useBreakpointValue({ base: true, lg: false })
 
-    return (
+    return isCollapseBreakpoint ? (
+        <Link href='/' aria-label='Go home' borderRadius='full'>
+            <Image
+                borderRadius='full'
+                boxSize='40px'
+                src={NotesAppIcon}
+                alt='Notes app logo' />
+        </Link>
+    ) : (
         <LinkBox rounded='md'>
-            <Grid templateColumns={goHomeGridBreakpoint} gap='15px' h='100%'>
+            <Grid templateColumns='40px 1fr' gap='15px'>
                 <Image
                     borderRadius='full'
                     boxSize='40px'
@@ -19,7 +26,7 @@ export const GoHome = () => {
                 <Box
                     d='flex'
                     alignItems='center'
-                    display={logoTextDisplayBreakpoint}>
+                    display='flex'>
                     <LinkOverlay href='/' aria-label='Go home'>
                         <Heading h='100%' as='h1' fontSize='26px'>
                             Best Notes
