2e12bd3d35f7070fac4ac3ef98ae65bfb9a5e07f
Vladislav Polishchuk <vladpolisuk159@gmail.com>
2021-12-25 13:47:48 +0300

    fix: fixed "useFormatedDate" hook

--- src/hooks/components/useFormatedDate.tsx
+++ src/hooks/components/useFormatedDate.tsx
@@ -1,12 +1,21 @@
 export function useFormatedDate(milleseconds: number) {
-    const date = new Date(milleseconds).toLocaleDateString('en-US');
+    const creationMilleseconds = milleseconds;
     const currentMilleseconds = Date.now();
+    const creationDate = new Date(creationMilleseconds).toLocaleDateString('en-US');
+    const currentDate = new Date(currentMilleseconds).toLocaleDateString('en-US');
+    const creationDayInMonth = new Date(creationDate).getDate();
+    const currentDayInMonth = new Date(currentDate).getDate();
+    const creationMonth = new Date(creationDate).getMonth() + 1;
+    const currentMonth = new Date(currentDate).getMonth() + 1;
+    const creationYear = new Date(creationDate).getFullYear();
+    const currentYear = new Date(currentDate).getFullYear();
 
-    const isToday = currentMilleseconds - milleseconds <= 86400000;
-    const isEsterday = (currentMilleseconds - milleseconds > 86400000)
-        && (currentMilleseconds - milleseconds <= 86400000 * 2)
+    const isToday = creationDate === currentDate;
+    const isEsterday = currentDayInMonth - creationDayInMonth === 1
+        && currentMonth === creationMonth
+        && currentYear === creationYear;
 
-    if (isToday) return 'Today';
-    if (isEsterday) return 'Yesterday';
-    else return date
+    if (isToday) return `Today (${currentDate})`;
+    else if (isEsterday) return `Yesterday (${creationDate})`;
+    else return creationDate;
 }
